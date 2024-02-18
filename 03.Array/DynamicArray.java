0beddb57a84c6c6bb567a4df78604e403ea9e6cf
Avishek Verma <avishekverma298@gmail.com>
2024-02-18 11:22:25 +0530

    Dynamic Array implementation compleated

--- 03.Array/DynamicArray.java
+++ 03.Array/DynamicArray.java
@@ -2,27 +2,35 @@ public class DynamicArray{
 
 	static int n;
 	static int idx=0;
-	// int[] arr;
+	static int [] arr = new int[10]; //Created and initilize the array for size 10;
 
-	static int [] arr = new int[10];
-
+	// Method to add an element at the end of the table.
 	public void add(int x) {
 
-		if(idx >= arr.length){
+		// Before adding element to array check for array size
+		// if array size is within its size then add the element at the end.
+		// else increase the size of array then add older elements to new array
+		// then add new element at the end of the array.
+		if(idx >= arr.length){ 
 			n=arr.length;
+			// Create new temp array to hold old elements.
 			int[] tempArr = new int[n];
+			// Add all elements of array to temp array.
 			int i=0;
 			for(int ele:arr){
 				tempArr[i++] = ele;
 			}
+			// Increase the array size my n/2
 			arr = new int[n+n/2];
+			// Add old elements to the increased array.
 			idx = 0;
 			for(int ele : tempArr){
 				arr[idx++]=ele;
 			}
+			// Add given element to the end of array.
 			arr[idx++]=x;
 		}else{
-			arr[idx++] = x;
+			arr[idx++] = x; // add element when size of array is in withing n.
 		}
 	}
 	
